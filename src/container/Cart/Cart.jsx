1db3f21a8e34c4232329b24b75d2a6d1e8710e4a
jorgezerpa <jorgezerpacoder@gmail.com>
2022-04-12 07:23:27 -0400

    all functionalities added to store and cart, next step: add contact form

--- src/container/Cart/Cart.jsx
+++ src/container/Cart/Cart.jsx
@@ -11,7 +11,8 @@ const Cart = () => {
     const classes = useStyles();
 
     const [totalPrice, setTotalPrice] = useState(0);
-
+    const [quantity, setQuantity] = useState(0)
+    const [toggler, setToggler] = useState(false) //for call useEffect and rerender page on each change
     const cart = new MyCart();
     const myCart = cart.getCart();
 
@@ -29,16 +30,25 @@ const Cart = () => {
 
     
     useEffect(()=>{
-        calculateTotalPrice()
-    }, [])
+        if(myCart) calculateTotalPrice()
+    }, [toggler])
 
+    
+    if(myCart.length===0 || !myCart){
+        return (
+            <div style={{paddingTop: '200px'}}>
+                <Typography gutterBottom variant='h1' textAlign='center'>Tu Compra</Typography>
+                <Typography variant='h2' textAlign='center'>Aún no has comprado nada, ¡QUÉ ESPERAS!</Typography>
+            </div>
+        )
+    }
 
     return (
         <div style={{paddingTop: '120px'}} className={classes.container}>
             <Typography gutterBottom variant='h1' textAlign='center'>Tu Compra</Typography>
             <Grid container className={classes.gridContainer} spacing={4} justifyContent='center' alignItems='center'>
-                {myCart.map((product, index)=>(
-                    <Grid key={index} id={index}  item className={classes.gridItem} xs={11} sm={11} md={10}>
+                {myCart.map((product, index)=>{
+                    return <Grid key={index} id={index}  item className={classes.gridItem} xs={11} sm={11} md={10}>
                         <Card className={classes.card}>
                             {/* <CardMedia className={classes.cardMedia}>
                                 <img className={classes.cardMediaImage} src={product && product.image} alt="" />
@@ -49,22 +59,35 @@ const Cart = () => {
                                 <Typography gutterBottom className={classes.subtitle} variant='body2'>cantidad: {product.quantity}</Typography>
                             </CardContent>
                             <CardActions>
-                                <Button>eliminar</Button>
-                                <Button><Icon><AddCircleIcon /></Icon></Button>
-                                <Button><Icon><RemoveCircleIcon /></Icon></Button>
+                                <Button onClick={(e)=>{
+                                    cart.removeFromCart(product.name)
+                                    setToggler(!toggler)
+                                    }}>eliminar</Button>
+                                <Button onClick={()=>{
+                                    cart.updateQuantity(product.name, product.price, product.quantity, 'sum', setQuantity);
+                                    setToggler(!toggler)
+                                }}><Icon><AddCircleIcon /></Icon></Button>
+                                <Button onClick={
+                                    ()=>{cart.updateQuantity(product.name, product.price, product.quantity, 'less', setQuantity);
+                                    setToggler(!toggler)
+                                    }}><Icon><RemoveCircleIcon /></Icon></Button>
                             </CardActions>
                         </Card>
                     </Grid>
-                ))}
+                })}
             </Grid>    
 
 
-
+            {myCart.length>0 &&
             <Card className={classes.card}>
                             <CardContent className={classes.cardContent}>
-                                <Typography className={classes.title} variant='h1'>precio total: { totalPrice }$</Typography>
+                                 <Typography className={classes.title} variant='h1'>precio total: { totalPrice }$</Typography>
                             </CardContent>
+                            <CardActions>
+                                <Button onClick={()=>{cart.clearCart(); window.location.reload()}}>Limpiar Carrito</Button>
+                            </CardActions>
             </Card>
+            }
         </div>
       )
 
